12224611f760ec47566dc081d6259cbeb2ff411d
Kathy Wang <kw496@cornell.edu>
2020-08-16 19:30:37 -0500

    fixed header

--- all.js
+++
@@ -1,6 +0,0 @@
-window.onload = () => {
-$("#nav a").on("click", function(){
-   $("#nav a").find(".active").removeClass("active");
-   $(this).parent().addClass("active");
-});
-}
---
+++ script.js
@@ -0,0 +1,30 @@
+
+// window.onload = () => {
+// // $("#nav a").on("click", function(){
+// //    $("#nav a").find(".active").removeClass("active");
+// //    $(this).parent().addClass("active");
+// // });
+//
+//
+// console.log('script.js says "I\'m here"');
+//
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
+//
+// // Get the header
+// var header = document.getElementById("myHeader");
+// console.log(document.getElementById("myHeader"));
+//
+// // Get the offset position of the navbar
+// var sticky = header.offsetTop;
+//
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
+//
+// }
