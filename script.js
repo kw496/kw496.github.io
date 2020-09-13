31c7556d70f0915e592909a48515d46434ea0459
Kathy Wang <kw496@cornell.edu>
2020-09-13 16:07:34 -0500

    nav id fixed

--- script.js
+++ script.js
@@ -13,17 +13,18 @@ window.onscroll = function() {myFunction()};
 
 // Get the header
 var header = document.getElementById("myHeader");
-console.log(document.getElementById("myHeader"));
+console.log(header);
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
+console.log( sticky );
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset > sticky) {
+  if ( window.pageYOffset > sticky ) {
     header.classList.add("sticky");
   } else {
-    header.classList.remove("sticky");
+    header.classList.remove( "sticky" );
   }
 }
 
