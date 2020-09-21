51ad69ed784aaca7eafbea057491a58a1d8d470d
Kathy Wang <kw496@cornell.edu>
2020-09-20 21:50:25 -0500

    got rid of js for now

--- script.js
+++ script.js
@@ -11,30 +11,30 @@ console.log('script.js says "I\'m here"');
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
-// Get the header
-var header = document.getElementById("myHeader");
-console.log(header);
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-console.log( sticky );
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if ( window.pageYOffset > sticky ) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove( "sticky" );
-  }
-}
-
-// Get the container element
-var btnContainer = document.getElementById("ul");
-console.log(btnContainer);
-
-// Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("nav-button");
-console.log(btns);
+// // Get the header
+// var header = document.getElementById("myHeader");
+// console.log(header);
+//
+// // Get the offset position of the navbar
+// var sticky = header.offsetTop;
+// console.log( sticky );
+//
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if ( window.pageYOffset > sticky ) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove( "sticky" );
+//   }
+// }
+//
+// // Get the container element
+// var btnContainer = document.getElementById("ul");
+// console.log(btnContainer);
+//
+// // Get all buttons with class="btn" inside the container
+// var btns = btnContainer.getElementsByClassName("nav-button");
+// console.log(btns);
 
 // // Loop through the buttons and add the active class to the current/clicked button
 // for (var i = 0; i < btns.length; i++) {
